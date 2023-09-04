d2404a7869ff134ef878ae348a715e9b41ff9f24
TempleTerry[x64] <123938029+PhilipPanda@users.noreply.github.com>
2023-09-04 18:13:33 +0200

    Add files via upload

---
+++ dots/templeOS/theme.bash
@@ -0,0 +1,91 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2023 Aditya Shakya <adi1090x@gmail.com>
+#
+# Default Theme
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#ffffff'
+foreground='#000000'
+color0='#101010'
+color1='#a80000'
+color2='#00a800'
+color3='#ffff57'
+color4='#0000a8'
+color5='#a800a8'
+color6='#57ffff'
+color7='#ffffff'
+color8='#101010'
+color9='#a80000'
+color10='#00a800'
+color11='#ffff57'
+color12='#0000a8'
+color13='#a800a8'
+color14='#57ffff'
+color15='#ffffff'
+
+accent='#0000a8'
+
+# Wallpaper
+wdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+wallpaper="$wdir/wallpaper"
+
+# Polybar
+polybar_font='TempleOS:size=6;2'
+
+# Rofi
+rofi_bg='#a8a8a8'
+rofi_font='TempleOS 6'
+rofi_icon='Zafiro'
+
+# Terminal
+terminal_font_name='TempleOS'
+terminal_font_size='6'
+
+# Geany
+geany_colors='slime.conf'
+geany_font='TempleOS 6'
+
+# Appearance
+gtk_font='TempleOS 6'
+gtk_theme='TempleOS'
+icon_theme='TempleOS'
+cursor_theme='TempleOS_Cursor'
+
+# Openbox
+ob_theme='TempleOS'
+ob_layout='MLC'
+ob_font='TempleOS'
+ob_font_size='6'
+ob_menu='menu-simple.xml'
+ob_margin_t='0'
+ob_margin_b='0'
+ob_margin_l='0'
+ob_margin_r='0'
+
+# Dunst
+dunst_width='200'
+dunst_height='50'
+dunst_offset='5x20'
+dunst_origin='top-right'
+dunst_font='TempleOS 6'
+dunst_border='2'
+dunst_separator='2'
+
+# Plank
+plank_hmode='auto'
+plank_offset='0'
+plank_position='bottom'
+plank_theme='Transparent'
+plank_icon_size='24'
+plank_zoom_percent='120'
+
+# Picom
+picom_backend='glx'
+picom_corner='0'
+picom_shadow_r='0'
+picom_shadow_o='0'
+picom_shadow_x='0'
+picom_shadow_y='0'
+picom_blur_method='none'
+picom_blur_strength='0'
